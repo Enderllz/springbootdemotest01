040f59c7b773d33aca75bbe8225da3164062a0bb
Ming <953971425cm@gmail.com>
2020-11-22 23:21:34 +0800

    create the project

---
+++ src/main/java/com/ming/springbootdemotest01/Springbootdemotest01Application.java
@@ -0,0 +1,13 @@
+package com.ming.springbootdemotest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemotest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootdemotest01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ming/springbootdemotest01/Springbootdemotest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.springbootdemotest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootdemotest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
