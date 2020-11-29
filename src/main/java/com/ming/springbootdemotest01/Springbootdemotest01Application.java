4b87315933aa89f7da5844f96d0a2db7edca9679
Enderllz <953971425@qq.com>
2020-11-30 01:48:00 +0800

    add Book.java

--- src/main/java/com/ming/springbootdemotest01/Springbootdemotest01Application.java
+++ src/main/java/com/ming/springbootdemotest01/Springbootdemotest01Application.java
@@ -8,6 +8,6 @@ public class Springbootdemotest01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Springbootdemotest01Application.class, args);
-    }
 
+    }
 }
