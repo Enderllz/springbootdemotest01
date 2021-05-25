1a6c7ad68c69b5ba8667c47e507aa241d75b0db0
Enderllz <953971425@qq.com>
2021-05-26 01:28:36 +0800

    fuck u

--- src/main/java/com/ming/springbootdemotest01/impl/BusinessPerson.java
+++ src/main/java/com/ming/springbootdemotest01/impl/BusinessPerson.java
@@ -3,11 +3,13 @@ package com.ming.springbootdemotest01.impl;
 import com.ming.springbootdemotest01.service.Animal;
 import com.ming.springbootdemotest01.service.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BusinessPerson implements Person {
     @Autowired
+    @Qualifier("dog")
     private Animal animal = null;
 
     @Override
--- src/main/java/com/ming/springbootdemotest01/impl/Cat.java
+++ src/main/java/com/ming/springbootdemotest01/impl/Cat.java
@@ -1,9 +1,11 @@
 package com.ming.springbootdemotest01.impl;
 
 import com.ming.springbootdemotest01.service.Animal;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Cat implements Animal {
     @Override
     public void use() {
